acfd233d2da5a26e3f888bca6360fc4d18046a74
Keith Levine <Levine.Keith@gmail.com>
2021-07-01 08:59:36 -0700

    connected to amazon s3 with boto3

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import boto3
+
+s3 = boto3.resource('s3')
 
 DEBUG = True
 PORT = 5000
@@ -8,6 +11,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'hi'
+
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
